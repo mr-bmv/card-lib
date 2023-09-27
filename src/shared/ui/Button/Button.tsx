89b20eade8b542c387058b492d975d8043bdfd1e
Mike Botkin <mr.m.botkin@gmail.com>
2023-09-27 09:44:12 +0300

    Button, Loader, Text

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,56 @@
+import { classNames } from '@/shared/lib/classNames/classNames';
+import cls from './Button.module.scss';
+import { ButtonHTMLAttributes, FC } from 'react';
+
+export enum ThemeButton {
+  CLEAR = 'clear',
+  CLEAR_INVERTED = 'clearInverted',
+  OUTLINE = 'outline',
+  OUTLINE_RED = 'outline_red',
+  BACKGROUND = 'background',
+  BACKGROUND_INVERTED = 'backgroundInverted',
+}
+
+export enum ButtonSize {
+  M = 'size_m',
+  L = 'size_l',
+  XL = 'size_xl',
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+  theme?: ThemeButton;
+  square?: boolean;
+  size?: ButtonSize;
+  disabled?: boolean;
+}
+
+export const Button: FC<ButtonProps> = (props) => {
+  const {
+    className = '',
+    children,
+    theme = ThemeButton.OUTLINE,
+    square = false,
+    disabled = false,
+    size = ButtonSize.M,
+    ...otherProps
+  } = props;
+
+  const mods: Record<string, boolean> = {
+    [cls[theme]]: true,
+    [cls.square]: square,
+    [cls[size]]: true,
+    [cls.disabled]: disabled,
+  };
+
+  return (
+    <button
+      type="button"
+      className={classNames(cls.Button, mods, [className])}
+      disabled={disabled}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
+};
---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,9 @@
+import './Loader.scss';
+
+export const Loader = () => (
+  <div className="loadingio-spinner-eclipse-9ehxkj606qu">
+    <div className="ldio-4jolxizg36o">
+      <div></div>
+    </div>
+  </div>
+);
